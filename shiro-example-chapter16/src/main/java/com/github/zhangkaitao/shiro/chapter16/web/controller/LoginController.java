ec866d27046f56e4bd7832231f53021d4e43252a
喻聪 <yucongcong@i-syp.com>
2019-10-26 17:09:28 +0800

    代码提交

--- shiro-example-chapter16/src/main/java/com/github/zhangkaitao/shiro/chapter16/web/controller/LoginController.java
+++ shiro-example-chapter16/src/main/java/com/github/zhangkaitao/shiro/chapter16/web/controller/LoginController.java
@@ -1,11 +1,7 @@
 package com.github.zhangkaitao.shiro.chapter16.web.controller;
 
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,28 +19,6 @@ public class LoginController {
     @RequestMapping(value = "/login"    )
     public String showLoginForm(HttpServletRequest req, Model model) {
         
-    	/*String error = null;
-        String username = req.getParameter("username");
-        String password = req.getParameter("password");
-        Subject subject = SecurityUtils.getSubject();
-        UsernamePasswordToken token = new UsernamePasswordToken(username, password);
-        token.setRememberMe(true);
-        try {
-            subject.login(token);
-        } catch (UnknownAccountException e) {
-        	e.printStackTrace();
-            error = "用户名/密码错误";
-        } catch (IncorrectCredentialsException e) {
-        	e.printStackTrace();
-            error = "用户名/密码错误";
-        } catch (AuthenticationException e) {
-        	e.printStackTrace();
-            //其他错误，比如锁定，如果想单独处理请单独catch处理
-            error = "其他错误：" + e.getMessage();
-        }*/
-    	
-    	
-    	
     	String exceptionClassName = (String)req.getAttribute("shiroLoginFailure");
     	String error = null;
         if(UnknownAccountException.class.getName().equals(exceptionClassName)) {
